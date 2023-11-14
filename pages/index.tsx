d75d0db9361806f434acd7712277b7b8968cb116
Abel-Mayowa <145891739+Abel-Mayowa@users.noreply.github.com>
2023-11-14 14:43:22 +0000

    fixed call stack exceeded

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Login from "./"
+import Login from "../pages/login.js"
 const Home: NextPage = () => {
   return (
     /*<div className={styles.container}>
