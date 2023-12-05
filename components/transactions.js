41a783ef439e59ad7607ac5db9ff44cd00cecb43
Abel-Mayowa <145891739+Abel-Mayowa@users.noreply.github.com>
2023-12-05 14:03:43 +0100

    Update transactions.js

--- components/transactions.js
+++ components/transactions.js
@@ -25,7 +25,8 @@ const Transactions = () => {
   const setData = useSetRecoilState(userData);
  // const csrf = useRecoilValue(csrfToken);
  // const setCsrf = useSetRecoilState(csrfToken); 
-  
+ window.localStorage.setItem(JSON.stringify(data));
+  const d =JSON.parse(window.localStorage.getItem("data"));
   useEffect(() => {  
    // if (!csrf) {
     //alert(4)
