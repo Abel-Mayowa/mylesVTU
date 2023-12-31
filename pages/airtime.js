61dac743657d3d96ca9b942dc492a08aeea92d6c
Abel-Mayowa <145891739+Abel-Mayowa@users.noreply.github.com>
2024-01-01 00:37:06 +0100

    Update airtime.js

--- pages/airtime.js
+++ pages/airtime.js
@@ -114,25 +114,32 @@ const Airtime = () => {
   return (
     <>
       <Head>
-      
-        <title>mylesVTU — buy cheap data,airtime and hire skilled web developer and Graphics designer </title>
-        <meta name="title" content="mylesVTU — buy cheap data,airtime and hire skilled web developer and Graphics designer " />
-        <meta name="description" content="Top up your mobile airtime effortlessly! Explore our airtime page for swift and secure recharges. Enjoy discounts on every top-up, ensuring seamless connectivity at affordable prices. Recharge with ease and stay connected with our exclusive offers." />
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buy Airtime Online - Instant Mobile Top-up | MylesVTU</title>
+    <meta name="description" content="Buy airtime instantly with ease. Top up your mobile phone credit and stay connected. Fast and secure airtime purchase at MylesVTU.">
+    <meta name="keywords" content="buy airtime, mobile top-up, instant airtime, online recharge, MylesVTU">
+    <meta name="author" content="MylesVTU">
+    <meta name="robots" content="index, follow">
+    <meta name="googlebot" content="index, follow">
+    <meta name="language" content="English">
+    <meta name="revisit-after" content="7 days">
+    <meta name="generator" content="Your CMS or Development Platform">
 
-        
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://metatags.io/" />
-        <meta property="og:title" content="mylesVTU — buy cheap data,airtime and hire skilled web developer and Graphics designer " />
-        <meta property="og:description" content="Top up your mobile airtime effortlessly! Explore our airtime page for swift and secure recharges. Enjoy discounts on every top-up, ensuring seamless connectivity at affordable prices. Recharge with ease and stay connected with our exclusive offers." />
-        <meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
-
-        
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://metatags.io/" />
-        <meta property="twitter:title" content="mylesVTU — buy cheap data,airtime and hire skilled web developer and Graphics designer " />
-        <meta property="twitter:description" content="Top up your mobile airtime effortlessly! Explore our airtime page for swift and secure recharges. Enjoy discounts on every top-up, ensuring seamless connectivity at affordable prices. Recharge with ease and stay connected with our exclusive offers." />
-        
-        
+    <!-- Open Graph meta tags for social sharing -->
+    <meta property="og:title" content="Buy Airtime Online - Instant Mobile Top-up | MylesVTU">
+    <meta property="og:description" content="Buy airtime instantly with ease. Top up your mobile phone credit and stay connected. Fast and secure airtime purchase at MylesVTU.">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://mylesvtu.com.ng/airtime">
+    <meta property="og:image" content="https://lh3.googleusercontent.com/p/AF1QipMgmurRxYZYbIeskHtFTD_iZ3GCEbxa8nHmEygE=s1348-w720-h1348">
+    
+    <!-- Twitter Card meta tags for Twitter sharing -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Buy Airtime Online - Instant Mobile Top-up | MylesVTU">
+    <meta name="twitter:description" content="Buy airtime instantly with ease. Top up your mobile phone credit and stay connected. Fast and secure airtime purchase at MylesVTU.">
+    <meta name="twitter:image" content="https://lh3.googleusercontent.com/p/AF1QipMgmurRxYZYbIeskHtFTD_iZ3GCEbxa8nHmEygE=s1348-w720-h1348">
+ 
         </Head>
       <Header />
       <ChakraProvider>
